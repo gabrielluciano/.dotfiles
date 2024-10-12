1303e994e805fe9b2c39134711cc5c3c541277ff
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-10-11 21:26:36 -0300

    delete old neovim config

--- nvim/.config/nvim/lua/config/keymaps.lua
+++
@@ -1,8 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
--- Dap
-require("which-key").add({
-    { "<leader>tR", "<ESC><CMD>lua require('dap').repl.open()<CR>", desc = "View Test Results", mode = "n" },
-})
